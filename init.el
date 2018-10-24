54e002f0267ba7e2fc5f988dc417ccfbea285352
yungcheeze <ucizim@gmail.com>
2018-10-24 11:33:44 +0100

    use python3, enable electric-pairs in org-mode

--- init.el
+++ init.el
@@ -325,9 +325,13 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  (setq org-agenda-files (quote ("~/org/agenda.org")))
+  ;; (setq org-agenda-files '( ("home/ucizi/org/agenda.org")
+  ;;                          ("home/ucizi/org/notes.org") ))
+  (setq python-shell-interpreter "python3")
   )
 
+;; Org-mode hooks
+(add-hook 'org-mode-hook 'electric-pair-mode)
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
 (defun dotspacemacs/emacs-custom-settings ()
