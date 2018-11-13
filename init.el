0efaf34ab7bb5590bca173d180ed313062d849ac
yungcheeze <ucizim@gmail.com>
2018-11-13 13:30:00 +0000

    udpate user config

--- init.el
+++ init.el
@@ -323,6 +323,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (setq clang-format-style "Google")
   )
 
 (defun dotspacemacs/user-config ()
