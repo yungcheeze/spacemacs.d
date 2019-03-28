4093171b336a1aa6a96c3973913a9886eca9072a
Ucizi Mafeni <ucizi.mafeni@turintech.ai>
2019-03-28 09:48:41 +0000

    change font

--- init.el
+++ init.el
@@ -173,9 +173,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
-                               :weight normal
+   dotspacemacs-default-font '("Ubuntu Mono"
+                               :size 16
                                :width normal
                                :powerline-scale 1.1)
    ;; The leader key
