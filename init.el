8097ec2453701c5436a2c3b148351c96e05711fb
Ucizi Mafeni <ucizi.mafeni@turintech.ai>
2019-04-03 16:07:18 +0100

    add json layer

--- init.el
+++ init.el
@@ -91,6 +91,7 @@ values."
      html
      javascript
      node
+     json
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
