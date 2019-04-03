56994d236b949313db3817c3bc7b9b29648e2d7e
Ucizi Mafeni <ucizi.mafeni@turintech.ai>
2019-04-03 12:21:23 +0100

    update agenda files

--- init.el
+++ init.el
@@ -353,7 +353,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-  (setq org-agenda-files '("/home/ucizi/org/agenda.org" "/home/ucizi/org/notes.org"))
+  (setq org-agenda-files '("/home/ucizi/Dropbox/org/agenda.org" "/home/ucizi/Dropbox/org/notes.org"))
   ;; (setq python-shell-interpreter "python3")
   (when (executable-find "ipython")
     (setq
