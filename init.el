7cdccf840de3a8d9b35c367af17a0837235e2cc9
Ucizi Mafeni <ucizi.mafeni@turintech.ai>
2019-04-10 15:18:58 +0100

    auto save when starting magit

--- init.el
+++ init.el
@@ -453,4 +453,5 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
+  (setq magit-save-repository-buffers 'dontask)
   )
