aab24d563e26a1170f3ef4f4fadf3fc31819ce64
Ucizi Mafeni <ucizi.mafeni@turintech.ai>
2019-06-07 16:05:25 +0100

    switch to microsoft python lsp

--- init.el
+++ init.el
@@ -59,7 +59,9 @@ This function should only modify configuration layer settings."
      (dap :variables
           dap-python-executable "python3")
      (python :variables
-             python-backend 'anaconda
+             python-backend 'lsp
+             python-lsp-server 'mspyls
+             python-lsp-git-root "~/tmp/python-language-server"
              python-test-runner 'pytest
              python-formatter 'yapf
              python-format-on-save t
